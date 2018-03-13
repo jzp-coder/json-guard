4326920a64c2b93ec21cb55029842f554a8373d1
iosifch <iosifpub+gthb@gmail.com>
2018-03-13 16:32:43 +0200

    init

---
+++ src/Foo.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace JGBundle;
+
+class FooTest
+{
+    private $name;
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public function name() : string
+    {
+        return $this->name;
+    }
+}
